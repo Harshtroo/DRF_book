891b15deb88a9a6c7de5dcef28d9146b4be2e445
harsh <harsh.vekariya@trootech.com>
2023-07-27 11:09:43 +0530

    add changes

--- book/book_crud/static/js/user_list.js
+++ book/book_crud/static/js/user_list.js
@@ -17,25 +17,3 @@ var resultData = ''
 
 makeAjaxCall(userListURL, methodType, resultData ,callBack)
 
-
-
-//$.ajax({
-//        type: 'GET',
-//        url: userListURL,
-//        dataType: 'json',
-//        success: function (data) {
-//            output = ""
-//            for (var i=0;i< data.length;i++){
-//                output += `
-//                            <tr>
-//
-//                                <td scope="row">${data[i].username}</td>
-//                                <td scope="row">${data[i].email}</td>
-//                                <td scope="row">${data[i].phone_number}</td>
-//                            </tr>`
-//            }
-//            document.getElementById("user_list").innerHTML = output
-//        },
-//        error: function (error){
-//        }
-//    })
