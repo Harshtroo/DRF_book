275b6a780a2bc3b87e0bae52ae85872b13c03e58
harsh <harsh.vekariya@trootech.com>
2023-08-18 19:07:08 +0530

    add test cases

--- book/book_crud/test/test_views.py
+++ book/book_crud/test/test_views.py
@@ -1,34 +1,85 @@
 from django.test import TestCase
+from django.urls import reverse
 from rest_framework.test import APIClient
+from book_crud.models import Book,Author
 from rest_framework import status
-from book_create.models import Book
+
 
 class CreateBookViewTest(TestCase):
-    def setUp(self):
-        self.client = APIClient()
 
-    def test_bulk_create_books(self):
-        data = {
-            "name": "Book1,Book2,Book3",
-            "author": "1,2,3",
-            # Add image data if needed
+    def setUp(self):
+        self.url = reverse("bulk_create_book")
+        self.client = APIClient()
+        Author.objects.create(name="David Beazley")
+        Author.objects.create(name="Wes Mckinney")
+        self.payload = {
+            "name": "Book1,Book2",
+            "author": "1,2",
+            "image":[
+                open("/home/ashishv/Djanog DRF bulk create/DRF_book/book/media/images/51j89lmxnpL._SX377_BO1204203200_.jpg","rb"),
+                open("/home/ashishv/Djanog DRF bulk create/DRF_book/book/media/images/51j89lmxnpL._SX377_BO1204203200_.jpg","rb"),
+            ]
+
         }
 
-        response = self.client.post('/path/to/your/endpoint/', data)
 
+    def test_create_book_with_valid_payload(self):
+        response = self.client.post(self.url, data=self.payload, format='multipart')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Book.objects.count(), 3)  # Check if 3 books were created
+        self.assertEqual(Book.objects.count(), 2)
 
-    def test_invalid_payload(self):
+    def test_create_book_with_invalid_payload(self):
+
         data = {
             "name": "Book1,Book2",
-            "author": "1,2,3",
-            # Add image data if needed
+            "author": "1",
+            "image": [
+                open(
+                    "/home/ashishv/Djanog DRF bulk create/DRF_book/book/media/images/51j89lmxnpL._SX377_BO1204203200_.jpg",
+                    "rb"),
+            ]
         }
-
-        response = self.client.post('/path/to/your/endpoint/', data)
-
+        response = self.client.post(self.url, data, format='multipart')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(Book.objects.count(), 0)  # No books should be created
+        self.assertEqual(Book.objects.count(), 0)
 
-    # Add more test cases if needed...
+    def test_single_create_book(self):
+        data = {
+            "name": "Book1",
+            "author": "1",
+            "image": [
+                open(
+                    "/home/ashishv/Djanog DRF bulk create/DRF_book/book/media/images/51j89lmxnpL._SX377_BO1204203200_.jpg",
+                    "rb"),
+            ]
+        }
+        response = self.client.post(self.url, data=data, format='multipart')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 1)
+
+    def test_create_book_with_missing_data(self):
+
+        data = {
+            "name": "Book1",
+            "author": "1",
+        }
+        response = self.client.post(self.url, data, format='multipart')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(Book.objects.count(), 0)
+
+    def test_create_book_with_invalid_data(self):
+
+        data = {
+            "name": "Book1,Book2",
+            "author": "1",
+            "image": [
+                open(
+                    "/home/ashishv/Djanog DRF bulk create/DRF_book/book/media/images/51j89lmxnpL._SX377_BO1204203200_.jpg",
+                    "rb"),
+            ]
+        }
+        response = self.client.post(self.url, data, format='multipart')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(Book.objects.count(), 0)
+
+
